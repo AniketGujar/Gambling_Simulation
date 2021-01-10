15ca1d1ac8cd15dd136ad608fbc24c2b98ca4681
AniketGujar <gujaraniket7875@gmail.com>
2021-01-10 15:42:37 +0530

    Added: Resign condition when player reach 50%(+/-) of stake

--- main.js
+++ main.js
@@ -2,18 +2,31 @@ console.log("Gambling Simulation Problem");
 
 let stake = 100;
 const BET = 1;
+const WIN_STAKE = 150;
+const LOSS_STAKE = 50;
+let flag = true;
+
 console.log("Player has total: " + stake + " $ ");
 
 class Gambler {
     gamble = () => {
-        let bet = Math.floor(Math.random() * 2);
-        if (bet == 0) {
-            stake++;
-            console.log("Player Won..!! \tPlayer has total: " + stake + " $ ");
+        while (flag) {
+            let bet = Math.floor(Math.random() * 2);
+            if (bet == 0) {
+                stake++;
+                console.log("Player Won..!!     Player has total: " + stake + " $ ");
+            }
+            else {
+                stake--;
+                console.log("Player Loss..!!    Player has total: " + stake + " $ ");
+            }
+            this.resign();
         }
-        else {
-            stake--;
-            console.log("Player Loss..!! \tPlayer has total: " + stake + " $ ");
+    }
+    resign() {
+        if (stake >= WIN_STAKE || stake <= LOSS_STAKE) {
+            console.log(" ----------------------------------------\n    Player Resign for the day with " + stake);
+            flag = false;
         }
     }
 }
