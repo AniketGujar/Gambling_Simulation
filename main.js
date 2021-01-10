74e85a43d579cebe32cc573b2eac51300c919668
AniketGujar <gujaraniket7875@gmail.com>
2021-01-10 17:46:11 +0530

    Added: Code to calculate total amount won or lost

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
 console.log("Gambling Simulation Problem");
 
 let stake = 100;
+let totalStake = 0;
+let totalAmount = 2000;
 const BET = 1;
 const WIN_STAKE = 150;
 const LOSS_STAKE = 50;
 let flag = true;
+const DAYS = 20;
+let stakeArray = [];
 
 console.log("Player has total: " + stake + " $ ");
 
 class Gambler {
     gamble = () => {
-        while (flag) {
-            let bet = Math.floor(Math.random() * 2);
-            if (bet == 0) {
-                stake++;
-                console.log("Player Won..!!     Player has total: " + stake + " $ ");
+        for (let day = 1; day <= DAYS; day++) {
+            stake = 100;
+            flag = true;
+            while (flag) {
+                let bet = Math.floor(Math.random() * 2);
+                if (bet == BET) {
+                    stake++;
+                }
+                else {
+                    stake--;
+                }
+                this.resign(day);
             }
-            else {
-                stake--;
-                console.log("Player Loss..!!    Player has total: " + stake + " $ ");
-            }
-            this.resign();
+        }
+        if (totalAmount > totalStake) {
+            console.log("Total Amount Loss " + (totalAmount - totalStake) + " $");
+        }
+        else {
+            console.log("Total Amount Loss " + (totalStake - totalAmount) + " $");
         }
     }
-    resign() {
+    resign(day) {
         if (stake >= WIN_STAKE || stake <= LOSS_STAKE) {
-            console.log(" ----------------------------------------\n    Player Resign for the day with " + stake);
+            console.log(" ----------------------------------------\nOn Day " + day + " Player Resign for the day with " + stake);
+            stakeArray[day - 1] = stake;
+            totalStake += stake;
             flag = false;
         }
     }
